273692e70e7994a826c393486e1e7da2c934e656
Alfredo Llamas <14206286+fllamas@users.noreply.github.com>
2020-12-11 00:30:09 -0800

    Add files via upload
    Added functions to write data to json files for transactions and keeping track of check numbers.

--- accounting.py
+++ accounting.py
@@ -1,15 +1,35 @@
 '''Accounting'''
 
 import locale
+import json
 
 # for currency formatting, set currency settings
 locale.setlocale(locale.LC_ALL, '')
 
+def update_check_num(check_count):
+    """Update the check number after transaction."""
+    check_count += 1
+
+    filename = 'check_number.json'
+    try:
+        with open(filename, 'w') as f:
+            json.dump(check_count, f)
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
+
+
 def get_check_num():
     """Returns the check number for the customer."""
-    check_count = 0 # use a file reader, for now temp.
+    filename = 'check_number.json'
+    try:
+        with open(filename) as f:
+            check_count = json.load(f)
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
 
-    return check_count + 1
+    update_check_num(check_count)
+
+    return check_count
 
 
 def total_order(order):
@@ -37,3 +57,19 @@ def process_payment(customer):
     print(f"Table: {customer.table_assigned}\tChk: #{customer.check_num:04d}")
     print(f"Your total amount is: {locale.currency(total, grouping=True)}")
     # add code later for payment options: cash, credit/debit card, mobile
+
+    # format customer info before writing transaction to file
+    customer_formated = {
+            'Table': f"{customer.table_assigned}",
+            'Chk': f"#{customer.check_num:04d}",
+            'Total amount': f"{locale.currency(total, grouping=True)}",
+            'Order': f"{customer.order}",
+            }
+
+    # write receipt to file
+    filename = 'transaction_history.json'
+    try:
+        with open(filename, 'a') as f:
+            json.dump(customer_formated, f, indent=4)
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
